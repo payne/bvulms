16fbed91e26af873f20d1624c3745aa10b25289d
Jason Shepherd <jason_b_shepherd@acm.org>
2014-07-31 23:59:04 -0500

    use process.env.DATABASE_URL if defined

--- models/db-util.js
+++ models/db-util.js
@@ -3,7 +3,8 @@ var env = 'dev';
 
 module.exports.query = function() {
     var q = require('pg-query');
-    q.connectionParameters = 'postgres://' + conf[env].user + ':' + conf[env].password + '@'
+    q.connectionParameters = process.env.DATABASE_URL ||
+                            'postgres://' + conf[env].user + ':' + conf[env].password + '@'
                             + conf[env].host + '/' + conf[env].database;
     return q;
 };
